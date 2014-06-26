8eb83de33e7cbd4a82e3c2215779fec7b9075e9d
svachere <samy@samyv.com>
2014-06-26 22:26:22 +0200

    added ret for checking return of exec

--- srcs/exec_node.c
+++ srcs/exec_node.c
@@ -6,7 +6,7 @@
 /*   By: apergens <apergens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/06/23 14:43:35 by svachere          #+#    #+#             */
-/*   Updated: 2014/06/26 22:08:23 by svachere         ###   ########.fr       */
+/*   Updated: 2014/06/26 22:25:55 by svachere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,21 +74,24 @@ int		exec_and_or(t_ast *ast, t_pipe pipes)
 
 int		exec_node(t_ast *ast, t_pipe pipes)
 {
+	int		ret;
+
+	ret = 1;
 	if (ast == NULL)
 		return (0);
 	if (ast->type == SEMIC)
-		exec_semic(ast);
+		ret = exec_semic(ast);
 	if (ast->type == AND || ast->type == OR)
-		exec_and_or(ast, pipes);
+		ret = exec_and_or(ast, pipes);
 	if (ast->type == PIPE)
-		exec_pipe(ast, pipes);
+		ret = exec_pipe(ast, pipes);
 	if (ast->type == REDAPP)
-		exec_redapp(ast, pipes);
+		ret = exec_redapp(ast, pipes);
 	if (ast->type == REDOUT)
-		exec_redout(ast, pipes);
+		ret = exec_redout(ast, pipes);
 	if (ast->type == REDIN)
-		exec_redin(ast, pipes);
+		ret = exec_redin(ast, pipes);
 	if (ast->type == STRING)
-		exec_string(ast, pipes);
-	return (1);
+		ret = exec_string(ast, pipes);
+	return (ret);
 }
